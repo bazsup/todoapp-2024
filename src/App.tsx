72364eae52ad13f93f24968cfa7ef6e528cadce6
bazsup <bas@odds.team>
2024-06-02 16:34:00 +0700

    extract home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import heroContent from "./assets/hero-content.jpg";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <main>
-        <div className="max-w-screen-lg flex mx-auto pt-10">
-          <div className="w-1/3 flex p-4 flex-col justify-center items-start text-left gap-6">
-            <h1 className="text-5xl leading-tight">
-              <span className="text-gray-500">
-                Simplest Todo App powered by{" "}
-              </span>
-              <span className="text-blue-500 font-bold line-he">React</span>
-            </h1>
-          </div>
-          <div className="w-2/3">
-            <img className="hero-image" src={heroContent} />
-          </div>
-        </div>
-      </main>
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,21 @@
+import heroContent from "../assets/hero-content.jpg";
+
+function Home() {
+  return (
+    <main>
+      <div className="max-w-screen-lg flex mx-auto pt-10">
+        <div className="w-1/3 flex p-4 flex-col justify-center items-start text-left gap-6">
+          <h1 className="text-5xl leading-tight">
+            <span className="text-gray-500">Simplest Todo App powered by </span>
+            <span className="text-blue-500 font-bold line-he">React</span>
+          </h1>
+        </div>
+        <div className="w-2/3">
+          <img className="hero-image" src={heroContent} />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
