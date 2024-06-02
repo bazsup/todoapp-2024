c3647f4e30e696bb15692c74ddfc80c26701ccce
bazsup <bas@odds.team>
2024-06-02 16:26:35 +0700

    introduce home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import heroContent from "./assets/hero-content.jpg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <div className="max-w-screen-lg text-right mx-auto flex justify-between py-4">
+          <div className="items-center text-2xl">📝 Todo</div>
+          <div className="flex gap-5 items-center justify-end">
+            <a href="">Contact</a>
+            <button className="rounded-full bg-blue-500 text-white px-4 py-2">
+              Login
+            </button>
+          </div>
+        </div>
+      </nav>
+      <main>
+        <div className="max-w-screen-lg flex mx-auto pt-10">
+          <div className="w-1/3 flex p-4 flex-col justify-center items-start text-left gap-6">
+            <h1 className="text-5xl leading-tight">
+              <span className="text-gray-500">
+                Simplest Todo App powered by{" "}
+              </span>
+              <span className="text-blue-500 font-bold line-he">React</span>
+            </h1>
+          </div>
+          <div className="w-2/3">
+            <img className="hero-image" src={heroContent} />
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
